9a3850df8827ef814a2103ea3a078d050b306afa
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 00:42:42 +0100

    css update(main_page): configure tag styles (add-ons)

--- frontend/components/commons/navbar.tsx
+++ frontend/components/commons/navbar.tsx
@@ -1,9 +1,18 @@
 import { Vault } from 'lucide-react';
 
-export default function Navbar() { 
+export default function Navbar() {
     return (
         <header>
-            <Vault size={10} color=''/>
+            <aside className='flex gap-2 text-primary'>
+                <Vault size={10} color='#EC7F13' />
+                <h2>OpenVault</h2>
+            </aside>
+            <ul className='md:flex gap-3 hidden'>
+                <li>protocol</li>
+                <li>USSD</li>
+                <li>Governance</li>
+                <li>docs</li>
+            </ul>
         </header>
     )
 }
