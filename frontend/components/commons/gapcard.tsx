5544f915cddb2c4cebc531a2ed4737b961eb8c0c
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 03:10:25 +0100

    update(main_page): added gapCard content and header styles

--- frontend/components/commons/gapcard.tsx
+++ frontend/components/commons/gapcard.tsx
@@ -4,8 +4,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 export default function GapCard({ key, header, icon, description }: {key: number, header: string, description: string, icon: JSX.Element}) {
     return (
-        <Card key={key} className="flex flex-col gap-2 justify-start p-3 w-full max-w-[300px] items-start">
-            <CardTitle>{icon}</CardTitle>
+        <Card key={key} className="flex flex-col gap-2 justify-start p-3 w-full max-w-[300px] items-start text-start">
+            <CardTitle className="p-2 border bg-secondary/40 ml-5 rounded">{icon}</CardTitle>
             <CardHeader className="text-nowrap text-center font-[600] text-lg w-full">{header}</CardHeader>
             <CardContent>{description}</CardContent>
         </Card>
