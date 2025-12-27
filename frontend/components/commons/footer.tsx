64b482b3a5d76b428886de2050f93bda2e16ac46
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 12:08:00 +0100

    update(main_page): refactor footer styles

--- frontend/components/commons/footer.tsx
+++ frontend/components/commons/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <main className="w-full py-8 px-6 bg-primary/20">
             <aside className="flex flex-col gap-3">
-                <div className='flex gap-1 text-primary items-center '>
-                    <Vault size={25} color='#EC7F13' />
+                <div className='flex gap-1 text-destructive items-center '>
+                    <Vault size={25} className="text-destructive" />
                     <h3>Openvault</h3>
                 </div>
                 <p className="max-w-[300px] text-start">The decentralized saving protocol for the offline world.EMpowering financial freedon withouth boundaries</p>
--- frontend/components/commons/navbar.tsx
+++ frontend/components/commons/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <header className="flex w-full px-3 py-2 sm:px-5 sm:py-4 justify-between items-center border-b-1 border-border">
             <aside className='flex gap-1 text-primary items-center group cursor-pointer' onClick={() => {router.push('/')}}>
-                <Vault size={25} color='#EC7F13' />
+                <Vault size={25} className="text-destructive" />
                 <h3>Openvault</h3>
             </aside>
             <ul className='md:flex gap-4 hidden'>
