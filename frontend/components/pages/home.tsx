1a3bf74a93be96f890347b483ba53598828407d3
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 02:55:43 +0100

    update(main_page): added aout card properties

--- frontend/components/pages/home.tsx
+++ frontend/components/pages/home.tsx
@@ -1,13 +1,44 @@
+import { Phone, PhoneCall } from "lucide-react";
 import Navbar from "../commons/navbar";
 import Hero from "./home/hero";
 
-export default function Home() { 
+export default function Home() {
+    const cards = [
+        {
+            icons: <PhoneCall />,
+            header: "Offline-first",
+            desc: 'No internet? No Problem. Our protocol is build on USSD Technology, compatible with 99% of mobile phones globally, even without data plans'
+        },
+        {
+            icons: <PhoneCall />,
+            header: "Non-Custodial",
+            desc: 'You hold the keys. Funds are stored in smart contracts only you control. We never have access to your savings'
+        },
+        {
+            icons: <PhoneCall />,
+            header: "Offline-first",
+            desc: 'No internet? No Problem. Our protocol is build on USSD Technology, compatible with 99% of mobile phones globally, even without data plans'
+        }
+    ]
     return (
-        <div className="w-full flex">
+        <div className="w-full flex flex-col">
             <main className="min-h-screen w-full flex flex-col">
                 <Navbar />
-                <Hero/>
+                <Hero />
             </main>
+            <section className="flex flex-col justify-center item-center text-center w-full">
+                <h4>Bridging the Gap</h4>
+                <p className="text-center w-full">Traditional banks exclude billions. internet-reliant Defi is inaccessible to many.
+                    <br />Openvault allows any mobile phone to interact directly with the blockchain
+                </p>
+                <article>
+                    {
+                        cards.map(() => (
+                            
+                        ))
+                    }
+                </article>
+            </section>
         </div>
     )
 }
