7af1539ad1289d8df66ec30fb0d812ea3d505c52
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 02:45:39 +0100

    update(main_page): refactor hero_footer styles

--- frontend/components/pages/home/hero.tsx
+++ frontend/components/pages/home/hero.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Hero() {
     return (
         <div className="flex flex-col gap-3 h-full justify-between items-center w-full">
-            <div className="flex flex-col md:flex-row gap-3 py-6 px-6 md:px-12 h-full justify-between items-center w-full">
-                <section className="flex flex-col gap-3 md:gap-8 w-1/2">
+            <div className="flex flex-col md:flex-row gap-3 sm:gap-6 py-6 px-6 md:px-12 h-full justify-between items-center w-full">
+                <section className="flex flex-col gap-3 md:gap-8 md:w-[40%]">
                     <p className="p-[3px_4px] rounded-lg flex gap-2 items-center max-w-[125px] text-sm bg-destructive/10 border-1 border-primary"><div className="w-2 h-2 rounded-[50%] bg-destructive" />Live on mainnet</p>
                     <article className="headers">
                         <h1 className="text-foreground/30">Offline.</h1>
@@ -30,45 +30,21 @@ export default function Hero() {
                         </article>
                     </div>
                 </section>
-                <section className="flex flex-col gap-3 md:gap-8 ">
-                    <p className="p-[3px_4px] rounded-lg flex gap-2 items-center max-w-[125px] text-sm bg-destructive/10 border-1 border-primary"><div className="w-2 h-2 rounded-[50%] bg-destructive" />Live on mainnet</p>
-                    <article className="headers">
-                        <h1 className="text-foreground/30">Offline.</h1>
-                        <h1 className="text-foreground/30">Permisionless.</h1>
-                        <h1 className="text-primary">Unstoppable.</h1>
-                    </article>
-                    <p className="max-w-[400px] sm:text-[0.85rem]">The saving protocol for the unbanked world. Access high-yield Defi vaults without an internet connection via USSD. Bank the unbanked, completely offline</p>
-                    <div className="flex gap-3">
-                        <Button variant={"destructive"}>Start Saving</Button>
-                        <Button variant={"outline"} >read whitepaper</Button>
-                    </div>
-                    <div className="flex gap-4 justify-start items-center text-[0.85rem] text-gray-500 dark:text-gray-300">
-                        <article className="flex gap-2">
-                            <input type="checkbox" checked className={"border p-2 rounded-[50%] bg-background shadow-xs hover:bg-accent "} />
-                            <p>Non-custodial</p>
-                        </article>
-                        <article className="flex gap-2">
-                            <input type="checkbox" checked className={"border p-2 rounded-[50%] bg-background shadow-xs hover:bg-accent"} />
-                            <p>Audited</p>
-                        </article>  <article className="flex gap-2">
-                            <input type="checkbox" checked className={"border p-2 rounded-[50%] bg-background shadow-xs hover:bg-accent"} />
-                            <p>Open-source</p>
-                        </article>
-                    </div>
+                <section className="h-full w-full border border-1 rounded-lg md:w-[40%]">
                 </section>
             </div>
             <div className="hero_footer">
                 <article>
-                    <h6>Total Value locked</h6>
-                    <h3>$14,203,941</h3>
+                    <p>Total Value locked</p>
+                    <h4>$14,203,941</h4>
                 </article>
                 <article>
                     <p>Active offline vaults</p>
-                    <h3>8,420</h3>
+                    <h4>8,420</h4>
                 </article>
                 <article>
                     <p>APY</p>
-                    <h3>~14%</h3>
+                    <h4>~14%</h4>
                 </article>
             </div>
         </div>
