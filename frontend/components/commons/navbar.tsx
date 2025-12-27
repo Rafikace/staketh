00ae18d55ede8bb00725cab3ddee0b4547446771
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-27 12:34:41 +0100

    update(main_page): update primary text style usage to destructive in navbar

--- frontend/components/commons/navbar.tsx
+++ frontend/components/commons/navbar.tsx
@@ -6,10 +6,10 @@ import { useRouter } from "next/navigation";
 export default function Navbar() {
     const router = useRouter();
     return (
-        <header className="flex w-full px-3 py-2 sm:px-5 sm:py-4 justify-between items-center border-b-1 border-border">
+        <header className="flex w-full px-3 py-2 sm:px-5 justify-between items-center border-b-1 border-border bg-destructive/10">
             <aside className='flex gap-1 text-primary items-center group cursor-pointer' onClick={() => {router.push('/')}}>
                 <Vault size={25} className="text-destructive" />
-                <h3>Openvault</h3>
+                <h3 className="text-destructive">Openvault</h3>
             </aside>
             <ul className='md:flex gap-4 hidden'>
                 <li>protocol</li>
